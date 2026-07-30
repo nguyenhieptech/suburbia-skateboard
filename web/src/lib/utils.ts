6db2018456ff72b53fc13c222949aa9304c43549
Hiep Nguyen <nguyenhieptech@gmail.com>
2026-07-30 20:09:46 +0700

    feat: add fluid-tailwindcss

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { twMerge } from "fluid-tailwindcss/tailwind-merge";
 
+// https://github.com/nguyenviet02/fluid-tailwindcss#tailwind-merge-integration
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
