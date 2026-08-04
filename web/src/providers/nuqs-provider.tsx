3b5b6df0a3ce5f75fd2452d2560b93b760fd2bf1
Hiep Nguyen <nguyenhieptech@gmail.com>
2026-08-04 19:49:18 +0700

    chore: add comment to nuqs

--- web/src/providers/nuqs-provider.tsx
+++ web/src/providers/nuqs-provider.tsx
@@ -1,5 +1,9 @@
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
+/**
+ * @description URL query string management with nuqs
+ * @see https://github.com/47ng/nuqs
+ */
 export function NuqsProvider({ children }: { children: React.ReactNode }) {
   return <NuqsAdapter>{children}</NuqsAdapter>;
 }
